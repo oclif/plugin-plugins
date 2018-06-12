8c646924f9d07ff0b00e99bd86e87c64646fd3ca
Shazron Abdullah <shazron@gmail.com>
2018-06-12 11:01:24 +0800

    Added clarifications to the behaviour of plugin:install and plugin:link
    Previously was in #45

--- src/commands/plugins/link.ts
+++ src/commands/plugins/link.ts
@@ -5,7 +5,12 @@ import cli from 'cli-ux'
 import Plugins from '../../plugins'
 
 export default class PluginsLink extends Command {
-  static description = 'links a plugin into the CLI for development'
+  static description = `links a plugin into the CLI for development
+Installation of a linked plugin will override a user-installed or core plugin.
+
+e.g. If you have a user-installed or core plugin that has a 'hello' command, installing a linked plugin with a 'hello' command will override the user-installed or core plugin implementation. This is useful for development work.
+`
+
   static usage = 'plugins:link PLUGIN'
   static examples = ['$ <%= config.bin %> plugins:link <%- config.pjson.oclif.examplePlugin || "myplugin" %> ']
   static args = [{name: 'path', description: 'path to plugin', required: true, default: '.'}]
