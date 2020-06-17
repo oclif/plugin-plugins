fcaffd90b827120ab172de4baee5c2f1e66dedb1
Titouan BION <titouan.bion@gmail.com>
2020-06-17 22:23:12 +0200

    fix: better print errors during plugins update hook (#77)

--- src/hooks/update.ts
+++ src/hooks/update.ts
@@ -4,5 +4,9 @@ import Plugins from '../plugins'
 
 export const update: Hook<'update'> = async function () {
   const plugins = new Plugins(this.config)
-  await plugins.update()
+  try {
+    await plugins.update()
+  } catch (error) {
+    this.error(error.message)
+  }
 }
