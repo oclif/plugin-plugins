1b4868f740dbc0136e64157d0f37717d21e86d73
Cristian Dominguez <cdominguez@salesforce.com>
2023-04-03 15:34:51 -0300

    fix: just check base prop

--- src/plugins.ts
+++ src/plugins.ts
@@ -352,9 +352,10 @@ export default class Plugins {
   private isValidPlugin(p: Config): boolean {
     if (p.valid) return true
 
-    // check if this plugin was loaded by `plugin-legacy`
     if (!this.config.plugins.find(p => p.name === '@oclif/plugin-legacy') ||
-        Object.getPrototypeOf(p) === 'PluginLegacy'
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore check if this plugin was loaded by `plugin-legacy`
+      p._base.includes('@oclif/plugin-legacy')
     ) {
       return true
     }
