fa8d63ebaafb5ccccd8375a8f5dd423b1feeb1bd
Mike Donnalley <mdonnalley@salesforce.com>
2023-09-08 09:45:57 -0600

    test: add tests for oclif.lock

--- test/commands/plugins/index.test.ts
+++ test/commands/plugins/index.test.ts
@@ -83,6 +83,29 @@ describe('command', () => {
     .do(output => expect(output.stdout).to.equal('No plugins installed.\n'))
     .it('does not install if unsure if scoped package does not exist')
   }
+
+  test
+  .command(['plugins:install', '@salesforce/plugin-custom-metadata'], {reset: true})
+  .stdout()
+  .command(['plugins'], {reset: true})
+  .do(output => expect(output.stdout).to.contain('custom-metadata'))
+  .command(['plugins:uninstall', '@salesforce/plugin-custom-metadata'])
+  .stdout()
+  .command(['plugins'], {reset: true})
+  .do(output => expect(output.stdout).to.equal('No plugins installed.\n'))
+  .it('installs and uninstalls plugin with oclif.lock')
+
+  test
+  .command(['plugins:install', '@salesforce/plugin-custom-metadata@2.2.0'], {reset: true})
+  .stdout()
+  .command(['plugins'], {reset: true})
+  .do(output => expect(output.stdout).to.contain('custom-metadata'))
+  .command(['plugins:uninstall', '@salesforce/plugin-custom-metadata'])
+  .stdout()
+  .command(['plugins'], {reset: true})
+  .do(output => expect(output.stdout).to.equal('No plugins installed.\n'))
+  .it('installs and uninstalls specific plugin version with oclif.lock')
+
   // test
   // .command(['plugins:install', 'heroku-debug@beta'], {reset: true})
   // .stdout()
