b0f8029389cc2f6380d29d9453eb3ebb057bc281
Jamie Feingold <jfeingold@salesforce.com>
2026-04-10 10:05:37 -0500

    experimenting

--- src/plugins.ts
+++ src/plugins.ts
@@ -243,7 +243,7 @@ export default class Plugins {
 
       await rm(join(this.config.dataDir, 'yarn.lock'), {force: true})
 
-      console.log(`package.json is ${await readFile(this.pjsonPath, 'utf-8')}`)
+      console.log(`package.json is ${await readFile(this.pjsonPath, 'utf8')}`)
       return plugin
     } catch (error: unknown) {
       this.debug('error installing plugin:', error)
