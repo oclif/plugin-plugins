179007c1c14aa52ffc378e008ae839231321d627
Eric Willhoit <ewillhoit@salesforce.com>
2026-09-08 09:47:10 -0500

    fix: bump npm

--- test/integration/install.integration.ts
+++ test/integration/install.integration.ts
@@ -187,7 +187,7 @@ describe('install/uninstall integration tests', () => {
 
   describe('multiple plugins sequentially', async () => {
     /**
-     * This is a test for @W-21915680@, a bizarre bug wherein if you installed a plugin from the registry by its true name,
+     * This is a test for W-21915680, a bizarre bug wherein if you installed a plugin from the registry by its true name,
      * and then installed a local tarball whose package name is alphabetically after the previous one, the local tarball
      * would silently fail to install.
      */
